c1b9df48174f02b900a010eb5795def9fe503911
hJun-KR <h0ngjunp.park@gmail.com>
2026-08-29 23:29:43 +0900

    test: main.cpp의 grayscale 테스트 주석 설명 수정

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,8 @@ int main(int argc, char** argv) {
     blank.write("blank.jpg");
     */
 
+    /**
+     * 이미지 그레이 스케일화(avg: 산술 평균 방식, lum: 인간의 시각 인지 가중치 방식)
     Image test("test1.jpg");
 
     Image gray_avg = test;
@@ -27,6 +29,11 @@ int main(int argc, char** argv) {
     Image gray_lum = test;
     gray_lum.grayscale_lum();
     gray_lum.write("gray_lum.png");
+    */
+
+    Image test("test.jpg");
+    test.colorMask(0, 0, 1);
+    test.write("blue.png");
 
 
 
